e866420daf2a50265973cc30983d753a5f5a17e1
sahuno <ekwame001@gmail.com>
2019-11-15 18:20:38 -0500

    testing functions: tell me your age and i will give you the  year you were born

---
+++ function_test.R
@@ -0,0 +1,11 @@
+###First text editpr  for R
+date <- "November 15th, 2019"
+print("Samuel Ahuno, author of thsi editor")
+
+##Tell me your age and I will tell you your date of birth
+age <- function (x) {
+  age <- 2019 - x
+  print(paste("your were born on", age, sep="-"))
+}  
+
+
